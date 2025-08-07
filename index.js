23cbdd57fcd60c306abb4243224c2d0ec696b467
PetronelSilviu <pmoisuc@bandwidth.com>
2025-08-07 13:07:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -80,3 +80,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.send('✅ Remote Agent Server is online and running!');
+});
